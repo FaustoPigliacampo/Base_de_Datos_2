9b8e15a76be1bf03b71a5536c198618f57dd6695
Fausto Pigliacampo <faustopiglia00@gmail.com>
2018-05-11 09:01:17 -0300

    HASTA_EL_3

--- Clase11.sql
+++ Clase11.sql
@@ -7,14 +7,26 @@ ORDER BY country, country_id;
 
 
 2)
-SELECT c.country_id, c.country, (SELECT COUNT(*) FROM city WHERE c.country_id = city.country_id) AS total
+SELECT c.country_id, c.country, COUNT(city) AS total
 FROM country c
-WHERE (SELECT COUNT(*) FROM city WHERE city.country_id = c.country_id) > 10
-ORDER BY (SELECT COUNT(*) FROM city WHERE city.country_id = c.country_id) DESC;
+INNER JOIN city
+	ON city.country_id = c.country_id
+GROUP BY c.country_id
+HAVING COUNT(city) > 10
+ORDER BY COUNT(city) DESC;
 
 
 3)
-Generate a report with customer (first, last) name, address, total films rented and the total money spent renting films. 
-
-
-Show the ones who spent more money first .
+SELECT customer.first_name, customer.last_name,
+		(SELECT address.address
+		FROM address
+		WHERE customer.address_id = address.address_id) AS address,
+		COUNT(*) AS rents,
+		(SELECT SUM(amount)
+		FROM payment
+		WHERE payment.customer_id = customer.customer_id) AS amount
+FROM customer
+INNER JOIN rental
+	ON rental.customer_id = customer.customer_id
+GROUP BY customer.customer_id
+ORDER BY amount DESC;
